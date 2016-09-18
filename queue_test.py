5c1f5003e08cfe8a0ea0017a60273e6eb7d1179a
Dan Jerome <djerome@gmail.com>
2016-09-17 23:00:51 -0400

    Added event queue and thread to service event queue to handle door server failure

---
+++ queue_test.py
@@ -0,0 +1,12 @@
+#!local/bin/python
+import threading
+import datetime
+        
+class ThreadClass(threading.Thread):
+  def run(self):
+    now = datetime.datetime.now()
+    print "%s says Hello World at time: %s" % (self.getName(), now)
+        
+for i in range(2):
+  t = ThreadClass()
+  t.start()
